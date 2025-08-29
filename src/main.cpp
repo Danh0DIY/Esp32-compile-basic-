7237935221658d0f7949defe094be61c7f7f15a0
Danh0DIY <dnn345169@gmail.com>
2025-08-29 21:44:44 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,41 @@
 #include <WiFi.h>
-#include <WebServer.h>
-#include <DNSServer.h>
-
-const char *ssid = "FreeWifi";   // Tên WiFi troll
-const char *password = "";
-
-WebServer server(80);
-DNSServer dns;
-
-void handleRoot() {
-  // Captive portal trả về redirect thẳng sang app
-  String page = "<!DOCTYPE html><html><head>";
-  page += "<meta http-equiv='refresh' content='0;url=intent://youtube.com/#Intent;scheme=https;package=com.google.android.youtube;end'>";
-  page += "</head><body></body></html>";
-  server.send(200, "text/html", page);
-}
+#include "esp_wifi.h"
 
 void setup() {
-  WiFi.mode(WIFI_AP);
-  WiFi.softAP(ssid, password);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_MODE_AP);
 
-  dns.start(53, "*", WiFi.softAPIP());   // bắt toàn bộ DNS
+  // Danh sách tên WiFi để troll
+  const char* ssids[] = {
+    "FreeWifi_100%",
+    "Mat_Khau_123456",
+    "Wifi_Nha_Hang_Xom",
+    "Hack_Phone_OK",
+    "Virus_Phone",
+    "ESP32_Troll_Mode",
+    "Anh_Yeu_Em",
+    "TikTok_Free",
+    "Zalo_Pro",
+    "Game_LienQuan_Lag"
+  };
 
-  server.onNotFound(handleRoot);
-  server.begin();
+  // Tạo nhiều AP ảo
+  for (int i = 0; i < 10; i++) {
+    wifi_config_t config;
+    memset(&config, 0, sizeof(wifi_config_t));
+    strcpy((char*)config.ap.ssid, ssids[i]);  // copy tên WiFi
+    config.ap.ssid_len = strlen(ssids[i]);
+    config.ap.channel = 1;
+    config.ap.authmode = WIFI_AUTH_OPEN;
+    config.ap.max_connection = 1;
+    config.ap.beacon_interval = 100;
+
+    esp_wifi_set_config(WIFI_IF_AP, &config);
+    esp_wifi_start();
+    delay(200);
+  }
 }
 
 void loop() {
-  dns.processNextRequest();
-  server.handleClient();
+  // không cần làm gì thêm, WiFi sẽ hiện ra
 }
